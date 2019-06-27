9d75b4f1c9e7387341a5014e3e477b397ae486da
lhq <lhq4696@outlook.com>
2019-06-27 22:17:46 +0800

    v0.1.2 coding...

---
+++ leetcode/leetcode21.h
@@ -0,0 +1,8 @@
+//
+// Created by Sanfuh on 2019/6/25.
+//
+
+#ifndef ALGORITHM_LEETCODE21_H
+#define ALGORITHM_LEETCODE21_H
+
+#endif //ALGORITHM_LEETCODE21_H
---
+++ leetcode/leetcode23.h
@@ -0,0 +1,8 @@
+//
+// Created by Sanfuh on 2019/6/25.
+//
+
+#ifndef ALGORITHM_LEETCODE23_H
+#define ALGORITHM_LEETCODE23_H
+
+#endif //ALGORITHM_LEETCODE23_H
